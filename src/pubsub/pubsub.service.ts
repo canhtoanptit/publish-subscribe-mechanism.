94fd7d13ccac9fc54a32392ccab012a2a47cf811
toannguyen <canhtoanptit@gmail.com>
2023-08-09 10:16:03 +0700

    add logic for pub sub service

--- src/pubsub/pubsub.service.ts
+++ src/pubsub/pubsub.service.ts
@@ -3,17 +3,27 @@ import {IEvent} from "../event/event.interface";
 import {ISubscriber} from "../subscribers/subscribe.interface";
 
 export class PublishSubscribeService implements IPublishSubscribeService {
-    private subscribers: Map<IEvent, Set<ISubscriber>> = new Map<IEvent, Set<ISubscriber>>();
+    private subscribers: Map<string, Set<ISubscriber>> = new Map<string, Set<ISubscriber>>();
 
     subscribe(eventType: string, subscriber: ISubscriber): void {
-
+        if (this.subscribers.has(eventType)) {
+            this.subscribers.get(eventType).add(subscriber);
+        } else {
+            this.subscribers.set(eventType, new Set<ISubscriber>([subscriber]));
+        }
     }
 
     unsubscribe(eventType: string, subscriber: ISubscriber): void {
-
+        if (this.subscribers.has(eventType)) {
+            this.subscribers.get(eventType).delete(subscriber);
+        }
     }
 
     publish(event: IEvent): void {
-
+        if (this.subscribers.has(event.eventType)) {
+            this.subscribers.get(event.eventType).forEach(s => {
+                s.handleEvent(event)
+            })
+        }
     }
 }
